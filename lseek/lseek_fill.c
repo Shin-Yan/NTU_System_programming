100fbffe14ede0698e11a1bf4d59d4eb7bfe872b
Shin-Yan <sdfghlklk@gmail.com>
2023-01-07 17:27:00 +0800

    update fileflags and lseek

--- lseek/lseek_fill.c
+++ lseek/lseek_fill.c
@@ -1,23 +1,21 @@
 #include<fcntl.h>
-#include<stdlib.h>
-
-#define FILE_MODE O_RDWR
+#include"apue.h"
 
 char buf1[] = "abcdefghij";
 char buf2[] = "ABCDEFGHIJ";
 char buf3[] = " ";
 int main(){
-    int fd;
+    int fd,i;
     if((fd = creat("file.fill", FILE_MODE))<0)
-        perror("create_error");
-    if(write(fd, buf1, 10)!= 10){
-        perror("buf1 write error");
-        return 0;
+        err_sys("create_error");
+    if(write(fd, buf1, 10)!= 10)
+        err_sys("buf1 write error");
+    for(i = 0 ; i < 16374 ; i++){
+        if(write(fd, buf3, 1)!=1) 
+            err_sys("buf3 write error");
     }
-    for(int i = 0 ; i < 16374 ; i++)
-        if(write(fd, buf3, 1)!=1) perror("buf3 write error");
     if(write(fd,buf2,10)!=10)
-        perror("buf2 write error");
+        err_sys("buf2 write error");
 
     exit(0);
 }
--- lseek/lseek_hole.c
+++ lseek/lseek_hole.c
@@ -1,7 +1,5 @@
 #include<fcntl.h>
-#include<stdlib.h>
-
-#define FILE_MODE O_RDWR
+#include"apue.h"
 
 char buf1[] = "abcdefghij";
 char buf2[] = "ABCDEFGHIJ";
@@ -9,13 +7,13 @@ char buf2[] = "ABCDEFGHIJ";
 int main(){
     int fd;
     if((fd = creat("file.hole", FILE_MODE))<0)
-        perror("create_error");
+        err_sys("create_error");
     if(write(fd, buf1, 10)!= 10)
-        perror("buf1 write error");
+        err_sys("buf1 write error");
     if(lseek(fd, 16384, SEEK_SET) == -1)
-        perror("lseek error");
+        err_sys("lseek error");
     if(write(fd,buf2,10)!=10)
-        perror("buf2 write error");
+        err_sys("buf2 write error");
 
     exit(0);
 }
